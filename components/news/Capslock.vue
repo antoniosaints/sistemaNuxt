add627c6f1e219e10fc4d7411fda99340ea2355c
antoniosaints <costaantonio883@gmail.com>
2022-08-02 17:51:40 -0300

    primeiro git

---
+++ components/news/Capslock.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-col cols="12">
+        <v-combobox v-model="select" :items="items" label="I use a scoped slot" multiple chips>
+            <template v-slot:selection="data">
+                <v-chip :key="JSON.stringify(data.item)" v-bind="data.attrs" :input-value="data.selected"
+                    :disabled="data.disabled" @click:close="data.parent.selectItem(data.item)">
+                    <v-avatar class="accent white--text" left v-text="data.item.slice(0, 1).toUpperCase()"></v-avatar>
+                    {{ data.item }}
+                </v-chip>
+            </template>
+        </v-combobox>
+    </v-col>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            select: ['Vuetify', 'Programming'],
+            items: ['Programming', 'Design', 'Vue', 'Vuetify'],
+        }
+    },
+}
+</script>
